88349c1d72b3db45ffdfce0b729e5aced504a523
Sandra Petereit <sandrap3393@googlemail.com>
2023-04-29 16:44:22 +0200

    #5 basic html & styling of landing page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,99 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <header>
+    <h1 class="app-name">What should I draw?</h1>
+  </header>
+  <main>
+    <section>
+      <div class="slides">
+        <div class="current-slide">
+          <img src="@/assets/slide-1.png" class="current-slide-img" />
+          <p class="current-slide-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            officiis voluptatem ut eaque omnis quaerat?
+          </p>
+        </div>
+        <div class="slides-indicator-bar">- - -</div>
+      </div>
+      <form class="login-form">
+        <button class="register-in-btn">Registrieren</button>
+        <button class="login-btn">Anmelden</button>
+      </form>
+    </section>
+    <section class="generator-section"></section>
+  </main>
+  <footer>By Tanja & Sandra</footer>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html {
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  font-size: 10px;
+  height: 100%;
+}
+
+:root {
+  --clr-purple01: #6949ff;
+  --clr-purple02: #bfb0ff;
+  --clr-yellow: #ffc107;
+  --clr-white: #ffffff;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+}
+</style>
+
+<style scoped>
+.app-name {
   text-align: center;
-  color: #2c3e50;
+  font-size: 5rem;
 }
 
-nav {
-  padding: 30px;
+.slides {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 2rem;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.current-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.current-slide-img {
+  width: 15rem;
+  height: 15rem;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.login-form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+section {
+  margin-bottom: 5rem;
+}
+
+footer {
+  height: 5rem;
+  width: 100%;
+  text-align: center;
+  font-size: 2rem;
+  background-color: var(--clr-purple01);
+  color: var(--clr-white);
 }
 </style>
