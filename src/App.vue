7dd775389f36c892d07bfc3a2c64a314ebc19a9a
Sandra Petereit <sandrap3393@googlemail.com>
2023-05-02 15:32:41 +0200

    #9: style of how-to section

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,41 @@
         <button class="login-btn">Anmelden</button>
       </form>
     </section>
+    <section class="how-to-section">
+      <h2 class="how-to-header">So verwendest du die App</h2>
+      <div class="how-to-container">
+        <img src="@/assets/checklist-transparent.png" class="how-to-images" />
+        <img src="@/assets/dice-transparent.png" class="how-to-images" />
+        <img src="@/assets/creativity-transparent.png" class="how-to-images" />
+        <span class="how-to-step-icon">1</span>
+        <span class="how-to-step-icon">2</span>
+        <span class="how-to-step-icon">3</span>
+        <span class="how-to-text"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
+          ducimus?</span
+        >
+        <span class="how-to-text"
+          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, odit
+          ab.</span
+        >
+        <span class="how-to-text"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          omnis voluptates eveniet?</span
+        >
+        <span class="how-to-explainations"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          mollitia ipsum eius.</span
+        >
+        <span class="how-to-explainations"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          mollitia ipsum eius.</span
+        >
+        <span class="how-to-explainations"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          mollitia ipsum eius.</span
+        >
+      </div>
+    </section>
     <section class="generator-section"></section>
   </main>
   <footer>By Tanja & Sandra</footer>
@@ -44,6 +79,7 @@ html {
   --clr-purple02: #bfb0ff;
   --clr-yellow: #ffc107;
   --clr-white: #ffffff;
+  --shadow-clr: #535353;
 }
 
 body {
@@ -84,8 +120,57 @@ body {
   flex-direction: column;
 }
 
+.how-to-section {
+  padding-block: 10rem;
+  background: url("@/assets/paint-stroke-background.png") no-repeat center 80%;
+  background-size: contain;
+}
+
+.how-to-header {
+  font-size: 4rem;
+}
+
+.how-to-container {
+  padding-block: 3rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+
+  font-size: 2rem;
+  text-align: center;
+}
+
+.how-to-step-icon {
+  background-color: var(--clr-purple01);
+  color: var(--clr-white);
+  font-size: 3rem;
+  padding: 1rem 2rem;
+  margin-block: 1rem;
+  border-radius: 100%;
+}
+
+.how-to-text {
+  width: 45ch;
+  margin-top: 1rem;
+}
+
+.how-to-explainations {
+  margin-top: 4rem;
+  padding: 1rem;
+  background-color: var(--clr-white);
+  border-radius: 1rem;
+  box-shadow: 0.25rem 0.25rem 0.5rem var(--shadow-clr);
+  width: 35rem;
+  height: 8rem;
+  font-size: 1.5rem;
+}
+
+.how-to-images {
+  width: 75%;
+}
+
 section {
-  margin-bottom: 5rem;
+  margin-bottom: 15rem;
 }
 
 footer {
