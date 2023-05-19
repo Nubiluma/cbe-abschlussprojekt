79f516462c5efa37590f6c7c0bf6dfc281f5d25d
teazy1910 <tanjaisabel.dietz@gmail.com>
2023-05-19 09:15:17 +0200

    delete slider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="slides">
-    <div class="current-slide">
+    <!-- <div class="current-slide">
       <img src="slide-1.png" class="current-slide-img" />
       <p class="current-slide-text">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
         officiis voluptatem ut eaque omnis quaerat?
       </p>
-    </div>
+    </div> -->
+    <Slider></Slider>
     <div class="slides-indicator-bar">- - -</div>
   </div>
   <form class="login-form">
-    <button @click="navigateToLogin" class="login-btn">Anmelden</button>
-    <button class="register-in-btn">Registrieren</button>
+    <button @click="navigateToLogin" class="login-btn btn">Anmelden</button>
+    <button class="register-in-btn btn">Registrieren</button>
   </form>
   <section class="how-to-section">
     <HowTo></HowTo>
@@ -31,18 +32,24 @@
 
 <script>
 import { useRouter } from "vue-router";
+import Slider from "../components/Slider.vue";
 
 export default {
   setup() {
     const router = useRouter();
-
     const navigateToLogin = () => {
       router.push("/LoginForm");
     };
-
     return {
       navigateToLogin,
     };
   },
+  components: { Slider },
 };
 </script>
+
+<style scoped>
+.btn {
+  padding: 0.8em 1em;
+}
+</style>
